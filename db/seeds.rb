0bd34647825ca7b2331ee7b1a9f21e8980d3a267
Adrien Loiseau <adrien.lois@gmail.com>
2020-08-11 16:59:41 +0200

    Ex.3

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts 'creating 10 fakes articles'
+Article.destroy_all
+
+10.times do
+  article = Article.new(
+    title: Faker::FunnyName.name,
+    content: Faker::ChuckNorris.fact
+  )
+  article.save!
+end
+
+puts 'seeding done'
